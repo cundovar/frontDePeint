669435961c960ea7a379b3d09f4380f4ed4d50a7
Facundo VARAS <102789450+cundovar@users.noreply.github.com>
2024-09-12 17:29:06 +0200

    firt

--- src/component/oeuvreid/coment.js
+++ src/component/oeuvreid/coment.js
@@ -3,13 +3,13 @@ import Accordion from '@mui/material/Accordion';
 import AccordionActions from '@mui/material/AccordionActions';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
-
+import CommentSercice from 'cundo-comment-service'
 import Button from '@mui/material/Button';
 import LesCommentaires from "../../pages/lesCommentaires";
 
 
 const Coments =()=>{
-
+ const apiUrl = "http://localhost:5000"
 return (
     <>
      <Accordion sx={{backgroundColor: "transparent", color:"white",border:"1px solid "}}>
@@ -21,7 +21,7 @@ return (
           <h1 className="text-xl font-bold">COMMENTAIRES</h1>
         </AccordionSummary>
         <AccordionDetails sx={{  }}>
-          <LesCommentaires/>
+        <CommentSercice apiUrl={apiUrl}/>
         </AccordionDetails>
       </Accordion>
      
